f59a6bcbb7aaa453e8763c1624aa4807dbbbcbb2
Victor Marri <victor.marri@dtidigital.com.br>
2021-04-05 17:35:22 -0300

    Controllers Criados e Conexão com o banco iniciada

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
+//Conecta ao banco
+mongoose.connect('mongodb+srv://victor:victor10@cluster0.0of6f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 //Carrega todas as rotas quue tivermos na aplicação
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
