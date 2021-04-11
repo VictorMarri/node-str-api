b8d786211f53a6e7d85e5de659607bde4ae93bf0
Victor Marri <victor.marri@dtidigital.com.br>
2021-04-11 00:11:23 -0300

    Controller 'getProductBySlug' adicionado

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,8 +4,25 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 //Rota Get dos produtos cadastrados
-exports.get = (req,res,next) => {
-    Product.find({ active: true }, 'title price slug') //Mostrar exatamente oq eu quero na resposta do json
+exports.getAllProducts = (req,res,next) => {
+    Product
+    .find({ 
+        active: true 
+    }, 'title price slug') //Mostrar exatamente oq eu quero na resposta do json
+    .then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+}
+
+
+exports.getProductBySlug = (req,res,next) => {
+    Product
+    .findOne({ 
+        slug: req.params.slug, 
+        active: true 
+    }, 'title description price slug tags') //Mostrar exatamente oq eu quero na resposta do json
     .then(data => {
         res.status(200).send(data);
     }).catch(e => {
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -4,7 +4,8 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/product-controller');
 
-router.get('/', controller.get);
+router.get('/', controller.getAllProducts);
+router.get('/:slug', controller.getProductBySlug);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.post('/', controller.delete);
